9922dad3d62baf914a571a82d15fb3cc8922c84b
Sandeep Kumar <sandeepqmar05@gmail.com>
2026-09-13 23:42:04 +0530

    Fix migrations

--- src/nasdaq/migrations.rs
+++ src/nasdaq/migrations.rs
@@ -3,7 +3,7 @@ use sea_orm_migration::prelude::*;
 use super::NasdaqTag;
 
 mod m00001_create_nasdaq;
-mod m00002_pg_trgm;
+mod m00002_nasdaq_pg_trgm;
 
 #[derive(Clone, Copy, Default)]
 pub struct Migrator;
@@ -13,7 +13,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m00001_create_nasdaq::Migration),
-            Box::new(m00002_pg_trgm::Migration),
+            Box::new(m00002_nasdaq_pg_trgm::Migration),
         ]
     }
 }
